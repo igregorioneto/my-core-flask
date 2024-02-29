480a389f0e7af27e011dece79fd371ee9459a9ea
igregorioneto <igregoriodoneto@outlook.com>
2024-02-29 09:29:00 -0300

    fix: base model

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -6,4 +6,4 @@ class BaseModel(db.model):
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at - db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
